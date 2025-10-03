dc677d7274af9f57f200edf802a399e41eedea92
Luis Bandeira <luisbandeira@brq.com>
2025-10-03 10:26:42 -0300

    adding fonts and icon with icomoon

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,16 +1,34 @@
-import { Box } from '@/src/components/Box';
+import { CityCard } from '@/src/components/CityCard';
+import { Icon } from '@/src/components/icon';
+import { Screen } from '@/src/components/Screen';
 import { Text } from '@/src/components/Text';
+import { cityPreviewList } from '@/src/data/cities';
 import { useAppTheme } from '@/src/theme/useAppTheme';
+import { CityPreview } from '@/src/type';
 import { useRouter } from 'expo-router';
+import { FlatList, ListRenderItemInfo } from 'react-native';
 
 export default function HomeScreen() {
   const router = useRouter()
   const { colors } = useAppTheme()
 
+  function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
+    return(
+      <CityCard cityPreview={item} />
+    )
+  }
+
   return (
-    <Box flex={1} backgroundColor='mainBackground'>
-      <Text margin='xl' color='text'>Home Screen</Text>
-    </Box>
+    <Screen>
+      <Text variant='title28'>Barcelona</Text>
+
+      <Icon name='Beach' color='text' />
+
+      <FlatList
+        data={cityPreviewList}
+        renderItem={renderItem}
+      />
+    </Screen>
   );
 }
 
---
+++ src/components/Screen.tsx
@@ -0,0 +1,11 @@
+
+import { PropsWithChildren } from "react";
+import { Box, BoxProps } from "./Box";
+
+export function Screen({children, ...boxProps}: PropsWithChildren & BoxProps) {
+    return (
+        <Box backgroundColor="background" paddingHorizontal="s16">
+            {children}
+        </Box>
+    )
+}
